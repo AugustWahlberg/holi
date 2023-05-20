5439d64834aed8f785e52f7c0dcebfa427fbf208
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-20 23:22:03 +0200

    Added form components

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,22 @@
+// FormInput.jsx
+import React from 'react';
+import * as S from '../pages/Homepage.Styles';
+
+
+const FormInput = ({ register, name, type = "text", placeholder, rules, errors }) => {
+  return (
+    <>
+      <S.Input
+        {...register(name, rules)}
+        placeholder={placeholder}
+        type={type}
+        style={{
+          border: errors ? '1px solid #BD121F' : '1px solid rgba(0, 153, 162, 1)',
+        }}
+      />
+      {errors && <S.ErrorMessage>{errors.message}</S.ErrorMessage>}
+    </>
+  );
+};
+
+export default FormInput;
---
+++ src/components/FormRole.jsx
@@ -0,0 +1,34 @@
+// FormRole.jsx
+import React from 'react';
+import * as S from '../pages/Homepage.Styles';
+
+const FormRole = ({ role, setRole, register }) => {
+  return (
+    <S.RoleContainer>
+      <S.RoleLabel isSelected={role === 'client'}>
+        <S.RoleInput
+          {...register('role')}
+          type="radio"
+          value="client"
+          checked={role === 'client'}
+          onChange={() => setRole('client')}
+        />
+        <S.CheckMark isSelected={role === 'client'} />
+        Client
+      </S.RoleLabel>
+      <S.RoleLabel isSelected={role === 'manager'}>
+        <S.RoleInput
+          {...register('role')}
+          type="radio"
+          value="manager"
+          checked={role === 'manager'}
+          onChange={() => setRole('manager')}
+        />
+        <S.CheckMark isSelected={role === 'manager'} />
+        Manager
+      </S.RoleLabel>
+    </S.RoleContainer>
+  );
+};
+
+export default FormRole;
