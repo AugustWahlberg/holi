cb8c63a96b09c3ce580d497986f6c5ad54efb5e8
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-20 23:22:48 +0200

    created dummy explore page

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import * as S from "./CreateVenue.Styles";
+
+
+function Explore() {
+
+  return (
+    <S.Container>
+      Explore
+  
+    </S.Container>
+   
+    );
+}
+
+export default Explore;
