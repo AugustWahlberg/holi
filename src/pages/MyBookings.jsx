10d8d587cc453ebaa2d8809f5f7a50dd1bc635b4
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-20 23:23:15 +0200

    created dummy MyBookings page

---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./CreateVenue.Styles";
+
+
+function MyBookings() {
+
+  return (
+    <S.Container>
+      My Bookings
+    </S.Container>
+    );
+}
+
+export default MyBookings;
