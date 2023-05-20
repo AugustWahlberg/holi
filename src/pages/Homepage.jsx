597e5b2be52e508d9a4d60e26f0a0edc0f14846d
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-20 23:23:58 +0200

    fixed register and login flow

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import * as S from './Homepage.Styles';
 import backgroundImage from '../images/hp-background.png';
 import logoSlogan from '../images/logo-w-slogan.png';
+import axios from 'axios';
+import FormRole from '../components/FormRole';
+import FormInput from '../components/FormInput';
+import FormSubmitButton from '../components/FormSubmit';
 
 function Homepage() {
+  const API_BASE_URL = 'https://api.noroff.dev/api/v1';
   const [isLogin, setIsLogin] = useState(true);
-  const [role, setRole] = useState('client'); // set default role
+  const [role, setRole] = useState('client');
+  const feedbackReg = useRef(null);
+  const feedbackLogin = useRef(null);
   const { register, handleSubmit, formState: { errors }, watch, reset } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const userRole = localStorage.getItem('role');
+
+    if (accessToken) {
+      if (userRole === "manager") {
+        navigate('/createVenue');
+      } else {
+        navigate('/explore');
+      }
+    }
+  }, [navigate]);
+
+  const onSubmit = async (data) => {
+    const endpoint = isLogin ? "/holidaze/auth/login" : "/holidaze/auth/register";
+    const url = `${API_BASE_URL}${endpoint}`;
+  
+    const requestData = {
+      email: data.email,
+      password: data.password,
+    };
+
+    if (!isLogin) {
+      requestData.name = data.username;
+      requestData.venueManager = role === "manager";
+      requestData.avatar = "https://news.artnet.com/app/news-upload/2021/09/Yuga-Labs-Bored-Ape-Yacht-Club-4466.jpg";
+    }
+  
+    const response = await postRequest(url, requestData);
+  
+    if (isLogin) {
+      if (response && response.accessToken) {
+        localStorage.setItem("accessToken", response.accessToken);
+        localStorage.setItem("role", response.venueManager ? "manager" : "client");
+        localStorage.setItem("username", response.name);
+        localStorage.setItem("email", response.email);
+        feedbackLogin.current.classList.remove("hidden");
+        feedbackLogin.current.classList.add("form-success");
+        feedbackLogin.current.classList.remove("form-error");
+        feedbackLogin.current.classList.add("hidden");
+        
+        setTimeout(() => {
+          if (response.venueManager) {
+            navigate('/createVenue');
+          } else {
+            navigate('/explore');
+          }
+        }, 1000);
+      } else {
+        feedbackLogin.current.classList.remove("hidden");
+        feedbackLogin.current.classList.remove("form-success");
+        feedbackLogin.current.classList.add("form-error");
+        feedbackLogin.current.innerHTML = "Invalid email or password";
+
+        setTimeout(() => {
+          feedbackLogin.current.classList.add("hidden");
+        }, 3000);
+      }
+    } else {
+      if (response && response.id) {
+        feedbackReg.current.classList.remove("hidden");
+        feedbackReg.current.classList.add("form-success");
+        feedbackReg.current.classList.remove("form-error");
+        feedbackReg.current.innerHTML = "Hey friend, your user has been created.";
+
+        setTimeout(() => {
+          feedbackReg.current.classList.add("hidden");
+          switchToLogin();
+        }, 4000);
+      } else {
+        feedbackReg.current.classList.remove("hidden");
+        feedbackReg.current.classList.remove("form-success");
+        feedbackReg.current.classList.add("form-error");
+        feedbackReg.current.innerHTML = "Sorry friend, the user already exists";
+
+        setTimeout(() => {
+          feedbackReg.current.classList.add("hidden");
+        }, 3000);
+      }
+    }
+  };
+
+  const postRequest = async (url, data) => {
+    try {
+      const response = await axios.post(url, data);
+      return response.data;
+    } catch (error) {
+      console.error(`Error during API call: ${error}`);
+    }
   };
 
   const isNoroffEmail = (value) => {
@@ -24,11 +120,17 @@ function Homepage() {
   const switchToLogin = () => {
     reset();
     setIsLogin(true);
+    if (feedbackReg.current) {
+      feedbackReg.current.classList.add("hidden");
+    }
   };
-
+  
   const switchToRegister = () => {
     reset();
     setIsLogin(false);
+    if (feedbackLogin.current) {
+      feedbackLogin.current.classList.add("hidden");
+    }
   };
 
   return (
@@ -51,91 +153,70 @@ function Homepage() {
 
         <S.Form onSubmit={handleSubmit(onSubmit)}>
           {!isLogin && (
-            <>
-              <S.RoleContainer>
-                <S.RoleLabel isSelected={role === 'client'}>
-                  <S.RoleInput
-                    {...register('role')}
-                    type="radio"
-                    value="client"
-                    checked={role === 'client'}
-                    onChange={() => setRole('client')}
-                  />
-                  <S.CheckMark isSelected={role === 'client'} />
-                  Client
-                </S.RoleLabel>
-                <S.RoleLabel isSelected={role === 'manager'}>
-                  <S.RoleInput
-                    {...register('role')}
-                    type="radio"
-                    value="manager"
-                    checked={role === 'manager'}
-                    onChange={() => setRole('manager')}
-                  />
-                  <S.CheckMark isSelected={role === 'manager'} />
-                  Manager
-                </S.RoleLabel>
-              </S.RoleContainer>
-
-              <S.Input
-                {...register('username', {
-                  required: true,
-                  minLength: 2,
-                })}
-                placeholder="Username"
-                style={{
-                  border: errors.username ? '1px solid #BD121F' : '1px solid rgba(0, 153, 162, 1)',
-                }}
-              />
-              {errors.username && <S.ErrorMessage>Minimum 2 characters required</S.ErrorMessage>}
-            </>
+            <FormRole role={role} setRole={setRole} register={register} />
           )}
-
-          <S.Input
-            {...register('email', {
-              required: true,
-              validate: isNoroffEmail,
-            })}
-            placeholder="Email"
-            style={{
-              border: errors.email ? '1px solid #BD121F' : '1px solid rgba(0, 153, 162, 1)',
-            }}
-          />
-          {errors.email && <S.ErrorMessage>Invalid Noroff email</S.ErrorMessage>}
-
-          <S.Input
-            {...register('password', {
-              required: true,
-              minLength: 8,
-            })}
-            placeholder="Password"
-            type="password"
-            style={{
-              border: errors.password ? '1px solid #BD121F' : '1px solid rgba(0, 153, 162, 1)',
-            }}
-          />
-          {errors.password && <S.ErrorMessage>Minimum 8 characters required</S.ErrorMessage>}
-
           {!isLogin && (
-            <>
-              <S.Input
-                {...register('confirmPassword', {
-                  required: true,
-                  validate: isPasswordMatch,
-                })}
-                placeholder="Confirm Password"
-                type="password"
-                style={{
-                  border: errors.confirmPassword ? '1px solid #BD121F' : '1px solid rgba(0, 153, 162, 1)',
-                }}
-              />
-              {errors.confirmPassword && <S.ErrorMessage>Password does not match</S.ErrorMessage>}
-            </>
+            <FormInput 
+              register={register}
+              name='username'
+              placeholder='Username'
+              rules={{ 
+                required: true, 
+                maxLength: { 
+                  value: 20, 
+                  message: 'Username cannot be more than 20 characters' 
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9_]+$/, 
+                  message: 'Username can only contain alphanumeric characters and underscores'
+                },
+              }}
+              errors={errors.username}
+            />
           )}
-
-          <S.SubmitButton type="submit">{isLogin ? 'Login' : 'Register'}</S.SubmitButton>
+          <FormInput 
+            register={register}
+            name='email'
+            placeholder='Email'
+            rules={{ 
+              required: 'Email is required', 
+              validate: value => isNoroffEmail(value) || 'Email has to be a @stud.noroff.no' 
+            }}
+            errors={errors.email}
+          />
+          <FormInput 
+            register={register}
+            name='password'
+            placeholder='Password'
+            type='password'
+            rules={{ 
+              required: 'Password is required', 
+              minLength: { 
+                value: 8, 
+                message: 'Password should be at least 8 characters' 
+              }
+            }}
+            errors={errors.password}
+          />
+          {!isLogin && (
+            <FormInput 
+              register={register}
+              name='confirmPassword'
+              placeholder='Confirm Password'
+              type='password'
+              rules={{ 
+                required: 'Confirm Password is required', 
+                validate: value => isPasswordMatch(value) || 'Passwords do not match' 
+              }}
+              errors={errors.confirmPassword}
+            />
+          )}
+          <FormSubmitButton isLogin={isLogin} />
+          <S.Feedback ref={feedbackReg} className="hidden"></S.Feedback>
+          <S.Feedback ref={feedbackLogin} className="hidden"></S.Feedback>
         </S.Form>
-        <S.VisitLink to="/createVenue">Visit website</S.VisitLink>
+
+        <S.VisitLink to="/explore">Visit website</S.VisitLink>
       </S.RightSide>
     </S.HomeContainer>
   );
