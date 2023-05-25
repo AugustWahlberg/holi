9d1a1464e302a6e01d72b1e34241cc30b1718ae1
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-26 01:07:41 +0200

    added skeleton for single venue page

--- src/pages/Venue.Styles.js
+++ src/pages/Venue.Styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { lighten } from 'polished';
 
 export const Container = styled.div`
   display: flex;
@@ -7,14 +8,138 @@ export const Container = styled.div`
   margin-top: 20px;
   margin-left: ${props => props.menuOpen ? '260px' : '0px'};
   transition: margin-left 0.3s ease-out;
+  
+  @media (max-width: 600px) {
+    margin-left: 0;
+  }
 `;
 
 export const Box = styled.div`
-  position: relative;
-  width: 80%;
-  min-height:440px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative; // Add this line to make the ButtonGroup absolutely positioned within the Box
   padding: 20px;
   margin: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.02);
+  width: 600px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border: 1px solid rgba(0, 49, 68, 0.1);
+  background: #ffffff;
+`;
+
+export const TopSide = styled.div`
+  width: 100%;
+`;
+
+export const BottomSide = styled.div`
+  width: 100%;
+`;
+
+export const Header = styled.h2`
+  margin-bottom: 10px;
+  color: #333;
+  font-weight: 700;
+`;
+
+export const Description = styled.p`
+  margin-bottom: 10px;
+  color: #666;
+`;
+
+export const Price = styled.p`
+  margin-bottom: 10px;
+  color: #333;
+  font-weight: bold;
+`;
+
+export const Rating = styled.p`
+  margin-bottom: 10px;
+  color: #666;
+`;
+
+export const MaxGuests = styled.p`
+  margin-bottom: 10px;
+  color: #666;
+`;
+
+export const Meta = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  color: #333;
+
+  svg {
+    margin-right: 5px;
+  }
+`;
+
+export const BackButton = styled.button`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  align-items: center;
+  background: #007BFF;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    margin-right: 5px;
+  }
+
+  &:hover {
+    background: ${lighten(0.1, '#007BFF')};
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BookButton = styled.button`
+  background: #007BFF;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background: ${lighten(0.1, '#007BFF')};
+  }
+`;
+
+export const ViewButton = styled.button`
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  background: #28a745;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    margin-right: 5px;
+  }
+
+  &:hover {
+    background: ${lighten(0.1, '#28a745')};
+  }
+`;
+
+export const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -1,19 +1,87 @@
 import React, { useState, useEffect } from "react";
-
 import * as S from "./Venue.Styles";
-
+import { FaWifi, FaParking, FaDog, FaBreadSlice, FaEye } from "react-icons/fa";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useParams, useNavigate } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
 
 function Venue({menuOpen}) {
+  const { id } = useParams(); 
+  const [venueData, setVenueData] = useState(null);
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.noroff.dev/api/v1/holidaze/venues/${id}`);
+        const data = await response.json();
+        setVenueData(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    
+    fetchData();
+  }, [id]);
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const handleView = () => {
+    console.log('View Button Clicked');
+  };
+
+  if (!venueData) {
+    return (
+      <S.SpinnerContainer>
+        <BeatLoader color="rgba(0, 49, 68, 0.8)" />
+      </S.SpinnerContainer>
+    );
+  }
 
   return (
     <S.Container menuOpen={menuOpen}>
+      <S.BackButton onClick={handleBack}>
+        Back
+      </S.BackButton>
       <S.Box>
+        <S.TopSide>
+          <Carousel>
+            {venueData.media.map((image, index) => 
+              <div key={index}>
+                <img src={image} alt="venue" />
+              </div>
+            )}
+          </Carousel>
+        </S.TopSide>
 
+        <S.BottomSide>
+          <S.Header>{venueData.name}</S.Header>
+          <S.Description>{venueData.description}</S.Description>
+          <S.Price>Price: {venueData.price}</S.Price>
+          <S.Rating>Rating: {venueData.rating}</S.Rating>
+          <S.MaxGuests>Max guests: {venueData.maxGuests}</S.MaxGuests>
+
+          <S.Meta>
+            {venueData.meta.wifi && <FaWifi />}
+            {venueData.meta.parking && <FaParking />}
+            {venueData.meta.breakfast && <FaDog />}
+            {venueData.meta.pets && <FaBreadSlice />}
+          </S.Meta>
+        </S.BottomSide>
+
+        <S.ButtonGroup>
+          <S.BookButton>Book Now</S.BookButton>
+          <S.ViewButton onClick={handleView}>
+            Availability
+          </S.ViewButton>
+        </S.ButtonGroup>
       </S.Box>
-  
     </S.Container>
-   
-    );
+  );
 }
 
 export default Venue;
