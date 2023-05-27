fb291962e3cd80520a39b732eb60922b9bdd8306
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-27 04:17:51 +0200

    Added modal

--- src/pages/Venue.Styles.js
+++ src/pages/Venue.Styles.js
@@ -1,17 +1,7 @@
 import styled from "styled-components";
-
-export const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin-top: 20px;
-  margin-left: ${props => props.menuOpen ? '260px' : '0px'};
-  transition: margin-left 0.3s ease-out;
-  
-  @media (max-width: 600px) {
-    margin-left: 0;
-  }
-`;
+import Modal from 'react-modal';
+import ReactDatePicker from 'react-datepicker';
+import { TbCircleArrowLeftFilled } from "react-icons/tb";
 
 export const Box = styled.div`
   display: flex;
@@ -25,27 +15,154 @@ export const Box = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border: 1px solid rgba(0, 49, 68, 0.1);
   background: #ffffff;
+
+
+  @media (max-width: 600px) {
+     width: 350px;
+  }
 `;
 
 export const TopSide = styled.div`
   width: 100%;
+  z-index:0;
 `;
-
 
 export const BottomSide = styled.div`
   width: 100%;
 `;
 
-export const Header = styled.h2`
-  margin-bottom: 10px;
+
+export const Meta = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
   color: #333;
-  font-weight: 700;
+
+  svg {
+    margin-right: 5px;
+  }
 `;
 
-export const Description = styled.p`
-  margin-bottom: 10px;
-  color: #666;
+export const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  color: #003144;
+  border-radius: 5px;
+  border: 1px solid #003144;
+  background: #fff;
+  cursor: pointer;
+  padding: 5px;
+  position: fixed;
+  top: 20px;
+  right: 40px;
+  z-index: 2;
 `;
+
+export const BackIcon = styled(TbCircleArrowLeftFilled)`
+align-items: center;
+padding-right: 4px;
+font-size: 18px;
+`;
+
+
+
+export const BookButton = styled.button`
+  background: #003144;
+  color: #fff;
+  padding: 10px 20px;
+  align-items: center;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+`;
+
+export const StyledModal = styled(Modal)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 350px;
+  background-color: #003144;;
+  border-radius: 10px;
+  padding: 20px;
+  outline: none;
+  z-index:100;
+`;
+
+
+export const InputGroup = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: rgba(51, 173, 181, 0.05);
+  border: 1px solid rgba(0, 153, 162, 0.35);
+  width: 300px;
+  color: #fff;
+  font-size: 14px;
+`;
+
+export const CloseModal = styled.button`
+  background: #003144;
+  color: #fff;
+  padding: 10px 20px;
+  align-items: center;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  background: rgba(189, 18, 31, 0.8);
+  margin-right: 10px;
+`;
+
+export const CreateBookingBtn = styled.button`
+background: #fff;
+color: #003144;
+  padding: 10px 20px;
+  align-items: center;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width:230px;
+`;
+
+export const DatePicker = styled(ReactDatePicker)`
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: rgba(51, 173, 181, 0.05);
+  border: 1px solid rgba(0, 153, 162, 0.35);
+  width: 300px;
+  color: #fff;
+  font-size: 14px;
+`;
+
+export const Header = styled.h2`
+  margin-bottom: 10px;
+  font-weight: 700;
+  margin-bottom:20px;
+`;
+
+export const ModalHeader = styled.h2`
+  margin-bottom: 10px;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+  margin-bottom:20px;
+  color: #ccc; 
+`;
+
 
 export const Price = styled.p`
   margin-bottom: 10px;
@@ -64,67 +181,31 @@ export const MaxGuests = styled.p`
   color: #666;
 `;
 
-export const Meta = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-  color: #333;
-
-  svg {
-    margin-right: 5px;
-  }
+export const Description = styled.p`
+  margin-bottom: 10px;
+  color: #666;
 `;
 
-export const BackButton = styled.button`
-  display: flex;
-  align-items: center;
-  color: #fff;
-  padding-left:5px;
-  border-radius: 5px;
-  border: 1px solid #003144;
-  color: #003144;
-  background: #fff;
-  cursor: pointer;
-  max-height: 30px;
-  display: block;
-  position: absolute;
-  top: 20px;
-  right: 40px;
-`;
 
-export const ButtonGroup = styled.div`
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
+export const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-export const BookButton = styled.button`
-  background: #003144;
-  color: #fff;
-  padding: 10px 20px;
-  align-items: center;
-  border-radius: 5px;
-  border: none;
-  cursor: pointer;
-
-`;
-
-export const AvailabilityButton = styled.button`
-  margin-top: 10px;
-  background-color: #fff;
-  padding: 10px;
-  border-radius: 5px;
-  border: none;
-  cursor: pointer;
-  border: 1px solid #003144;
-`;
-
-export const SpinnerContainer = styled.div`
-  display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  text-align: center;
+  z-index: 100;
+  background-color: none;
 `;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  z-index: 10000;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
 
--- src/pages/Venue.jsx
+++ src/pages/Venue.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import * as S from "./Venue.Styles";
-import { FaWifi, FaParking, FaDog, FaBreadSlice, FaEye } from "react-icons/fa";
-import { TbCircleArrowLeftFilled } from "react-icons/tb";
+import * as CS from "./CommunComponents.Styles";
+import { FaWifi, FaParking, FaDog, FaBreadSlice } from "react-icons/fa";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useParams, useNavigate } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
+import { registerLocale } from  "react-datepicker";
+import enGB from 'date-fns/locale/en-GB';
+registerLocale('en-GB', enGB)
 
 function Venue({menuOpen}) {
 
-  // Define state for current image
-const [currentImage, setCurrentImage] = useState(0);
-
-// Define callback to handle slide change
-const handleSlideChange = (index) => {
-  setCurrentImage(index);
-}
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [guests, setGuests] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const { id } = useParams(); 
   const [venueData, setVenueData] = useState(null);
-
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -31,7 +31,7 @@ const handleSlideChange = (index) => {
         console.error(err);
       }
     };
-    
+
     fetchData();
   }, [id]);
 
@@ -39,64 +39,146 @@ const handleSlideChange = (index) => {
     navigate(-1);
   };
 
-  const handleView = () => {
-    console.log('View Button Clicked');
+  const handleSlideChange = (index) => {
+    setCurrentImage(index);
+  }
+
+  const handleBook = async () => {
+    if (!startDate || !endDate || !guests) {
+      console.error('Please fill all fields.');
+      return;
+    }
+
+    const response = await fetch('https://api.noroff.dev/api/v1/holidaze/bookings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        dateFrom: startDate.toISOString(),
+        dateTo: endDate.toISOString(),
+        guests: guests,
+        venueId: id,
+      }),
+    });
+
+    if (response.ok) {
+      const bookingData = await response.json();
+      console.log('Booking created successfully:', bookingData);
+      setModalIsOpen(false);  // close the modal after successful booking
+    } else {
+      console.error('Failed to create booking.');
+    }
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   if (!venueData) {
     return (
-      <S.SpinnerContainer>
+      <CS.SpinnerContainer menuOpen={menuOpen}>
         <BeatLoader color="rgba(0, 49, 68, 0.8)" />
-      </S.SpinnerContainer>
+      </CS.SpinnerContainer>
     );
   }
 
-  return (
+
+    return (
+      <>
+      <CS.Top  />
+      
+      <CS.Container menuOpen={menuOpen}>
     
-    <S.Container menuOpen={menuOpen}>
-
-<S.BackButton onClick={handleBack}>
-      <TbCircleArrowLeftFilled></TbCircleArrowLeftFilled>
-        Back
-      </S.BackButton>
-
-     
-      <S.Box>
-        <S.TopSide>
-        <Carousel onChange={handleSlideChange}>
-  {venueData.media.map((image, index) => 
-    <div key={index} style={{ height: '500px', overflow: 'hidden' }}>
-      <img src={image} alt="venue" style={{ height: '100%', width: 'auto' }} />
-    </div>
-  )}
-</Carousel>
-
-        </S.TopSide>
-
-        <S.BottomSide>
-          <S.Header>{venueData.name}</S.Header>
-          <S.Description>{venueData.description}</S.Description>
-          <S.Price>{venueData.price}$</S.Price>
-          <S.Rating>Rating: {venueData.rating}</S.Rating>
-          <S.MaxGuests>Max guests: {venueData.maxGuests}</S.MaxGuests>
-
-          <S.Meta>
-            {venueData.meta.wifi && <FaWifi />}
-            {venueData.meta.parking && <FaParking />}
-            {venueData.meta.breakfast && <FaDog />}
-            {venueData.meta.pets && <FaBreadSlice />}
-          </S.Meta>
-        </S.BottomSide>
-
-        <S.ButtonGroup>
-          <S.BookButton>Book Now</S.BookButton>
-          <S.AvailabilityButton onClick={handleView}>
-            Availability
-          </S.AvailabilityButton>
-        </S.ButtonGroup>
-      </S.Box>
-    </S.Container>
-  );
+        <S.BackButton onClick={handleBack}>
+          <S.BackIcon/>
+          Back
+        </S.BackButton>
+    
+        <S.Box>
+          <S.TopSide>
+            <Carousel onChange={handleSlideChange}>
+              {venueData.media.map((image, index) => 
+                <div key={index} style={{ height: '500px', overflow: 'hidden' }}>
+                  <img src={image} alt="venue" style={{ height: '100%', width: 'auto' }} />
+                </div>
+              )}
+            </Carousel>
+          </S.TopSide>
+    
+          <S.BottomSide>
+            <S.Header>{venueData.name}</S.Header>
+            <S.Description>{venueData.description}</S.Description>
+            <S.Price>{venueData.price}$</S.Price>
+            <S.Rating>Rating: {venueData.rating}</S.Rating>
+            <S.MaxGuests>Max guests: {venueData.maxGuests}</S.MaxGuests>
+    
+            <S.Meta>
+              {venueData.meta.wifi && <FaWifi />}
+              {venueData.meta.parking && <FaParking />}
+              {venueData.meta.breakfast && <FaDog />}
+              {venueData.meta.pets && <FaBreadSlice />}
+            </S.Meta>
+          </S.BottomSide>
+    
+          <S.ButtonGroup>
+            <S.BookButton onClick={openModal}>Book Now</S.BookButton>
+          </S.ButtonGroup>
+        </S.Box>
+      </CS.Container>
+    
+      <S.StyledModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Booking Modal"
+      >
+        <S.ModalContent>
+          <S.ModalHeader>Create a booking</S.ModalHeader>
+          <S.InputGroup>
+            <S.Input
+              type="number"
+              min="0"
+              placeholder="Number of guests"
+              onChange={event => setGuests(event.target.value)}
+            />
+    
+            <S.DatePicker
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="Start date"
+              dateFormat="yyyy-MM-dd"
+              locale="en-GB"
+            />
+    
+            <S.DatePicker
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              placeholderText="End date"
+              dateFormat="yyyy-MM-dd"
+              locale="en-GB"
+            />
+          </S.InputGroup>
+    
+          <S.ButtonGroup>
+            <S.CloseModal onClick={closeModal}>Close</S.CloseModal>
+            <S.CreateBookingBtn onClick={handleBook}>Book now</S.CreateBookingBtn>
+          </S.ButtonGroup>
+        </S.ModalContent>
+      </S.StyledModal>
+      </>
+    );
+    
 }
 
 export default Venue;
