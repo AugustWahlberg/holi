7517e5c9e676e058affed5b08d563739db6f1621
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-27 21:51:51 +0200

    Added styling to My Bookings page

--- src/pages/MyBookings.Styles.js
+++ src/pages/MyBookings.Styles.js
@@ -1,12 +1,106 @@
 import styled from "styled-components";
 
-
-export const Container = styled.div`
+export const Box = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-  justify-content: center;
-  background-color: red;
-  margin-left: ${props => props.menuOpen ? '260px' : '0px'};
-  transition: margin-left 0.3s ease-out;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  position: relative;
+  padding: 20px;
+  margin: 20px;
+  width: 350px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 1px solid rgba(0, 49, 68, 0.1);
+  background: #ffffff;
+  @media (max-width: 900px) {
+    width: 300px;
+    height: auto;
+  }
 `;
+
+export const BookingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const BookingInfo = styled.div`
+  padding-left: 20px; 
+`;
+
+export const BookingImage = styled.div`
+  width: 100%;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover; 
+`;
+
+export const ButtonsContainer = styled.div`
+  position: absolute;
+  bottom: 10px;
+  right: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+
+export const Button = styled.button`
+  padding: 8px;
+  font-size: 14px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  width: 100px;
+  margin: 6px;
+`;
+
+
+export const ViewVenue = styled(Button)`
+
+background: linear-gradient(135deg, #F16FBE, #003144);
+color: #fff;
+opacity: 0.8;
+
+&:hover {
+  opacity: 1;
+  }
+ 
+`;
+
+export const EditButton = styled(Button)`
+background-color: rgba(0, 49, 68, 0.8);
+color: #fff;
+&:hover {
+  background-color: rgba(0, 49, 68, 1);
+}
+`;
+
+
+export const DeleteButton = styled(Button)`
+  background-color: rgba(189, 18, 31, 0.8);
+  color: #fff;
+  &:hover {
+    background-color: rgba(189, 18, 31, 1);
+  }
+`;
+
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  margin: 3px;
+  margin-top: 6px;
+  font-weight: 500;
+  
+  svg {
+    font-size: 16px;
+    margin-right: 8px;
+  }
+`;
+
--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,16 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { TbBrowserCheck, TbDoorEnter, TbDoorExit, TbUsers} from "react-icons/tb";
+import { BeatLoader } from "react-spinners";
 
 import * as S from "./MyBookings.Styles";
 import * as CS from "./CommunComponents.Styles";
 
 function MyBookings({ menuOpen }) { // accept menuOpen prop here
+  const [bookings, setBookings] = useState([]);
+  const [username, setUsername] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <CS.Container menuOpen={menuOpen}> 
-    My Bookings
-  </CS.Container>
-  );
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      const token = localStorage.getItem("accessToken");
+      if (!token) {
+        console.log('User not logged in');
+        return;
+      }
+      
+      const response = await fetch(`https://api.noroff.dev/api/v1/holidaze/bookings?_venue=true`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+      const data = await response.json();
+      console.log('API response:', data);
+      setBookings(data);
+      setLoading(false)
+    };
+    
+
+    const user = localStorage.getItem("username");
+    if (user) {
+      setUsername(user);
+      fetchData();
+    } else {
+      console.log('No user found in local storage');
+    }
+  }, [username]);
+
+  if (loading) {
+    return (
+      <CS.SpinnerContainer menuOpen={menuOpen}>
+          <BeatLoader color="rgba(0, 49, 68, 0.8)" />
+        </CS.SpinnerContainer>
+    );
 }
 
+  return (
+    <CS.Container menuOpen={menuOpen}>
+      
+      <CS.Header>My Bookings</CS.Header>
+      {Array.isArray(bookings) ? (
+  bookings.map((booking, index) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const dateFrom = new Date(booking.dateFrom).toLocaleDateString();
+    const dateTo = new Date(booking.dateTo).toLocaleDateString();
+    const created = new Date(booking.created).toLocaleDateString();
+
+    return (
+      <S.Box key={index}>
+      <S.BookingImage>
+        <S.Image src={booking.venue.media[0]} alt="Venue" />
+      </S.BookingImage>
+      <S.BookingContainer>
+        <S.BookingInfo>
+        <h2> {booking.venue.name}</h2>
+        <S.Info><TbDoorEnter /> {dateFrom}</S.Info>
+        <S.Info><TbDoorExit /> {dateTo}</S.Info>
+        <S.Info><TbUsers /> {booking.guests}</S.Info>
+        <S.Info><TbBrowserCheck />{created}</S.Info>
+        </S.BookingInfo>
+        <S.ButtonsContainer>
+          <S.ViewVenue>View Venue</S.ViewVenue>
+          <S.EditButton>Edit</S.EditButton>
+          <S.DeleteButton>Delete</S.DeleteButton>
+        </S.ButtonsContainer>
+      </S.BookingContainer>
+    </S.Box>
+    );
+  })
+) : (
+  <p>No bookings found.</p>
+)}
+
+    </CS.Container>
+  );
+}
 
 export default MyBookings;
