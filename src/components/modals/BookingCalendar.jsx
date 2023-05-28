aa256d4afc1d9b6e974b7cd54b6075ec2f5cb4a9
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2023-05-28 19:56:37 +0200

    fixed availabilty to the book calender

---
+++ src/components/modals/BookingCalendar.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { DatePicker } from './Modals.Styles';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function BookingCalendar({ venueId, startDate, endDate, setStartDate, setEndDate }) {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const response = await fetch(
+          `https://api.noroff.dev/api/v1/holidaze/venues/${venueId}?_bookings=true`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setBookings(data.bookings);
+        }
+      } catch (error) {
+        console.error('Error fetching bookings:', error);
+      }
+    };
+
+    fetchBookings();
+  }, [venueId]);
+
+  const isBookedDate = (date) => {
+    const formattedDate = date.toISOString().split('T')[0];
+    return bookings.some((booking) => {
+      const bookingStartDate = booking.dateFrom.split('T')[0];
+      const bookingEndDate = booking.dateTo.split('T')[0];
+      return formattedDate >= bookingStartDate && formattedDate <= bookingEndDate;
+    });
+  };
+
+  const today = new Date(); // Get the current date
+
+  return (
+    <>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        placeholderText="Start date"
+        dateFormat="yyyy-MM-dd"
+        locale="en-GB"
+        minDate={today} // Set the minimum selectable date to today
+        filterDate={isBookedDate}
+      />
+
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+        placeholderText="End date"
+        dateFormat="yyyy-MM-dd"
+        locale="en-GB"
+        filterDate={isBookedDate}
+      />
+    </>
+  );
+}
+
+export default BookingCalendar;
--- src/components/modals/BookingModal.jsx
+++ src/components/modals/BookingModal.jsx
@@ -1,80 +1,108 @@
-import React, { useState } from "react";
-import * as MS from "./Modals.Styles";
-import { handleBook } from "../../api/HandleBook";
+import React, { useState } from 'react';
+import * as MS from './Modals.Styles';
+import { handleBook } from '../../api/HandleBook';
+import BookingCalendar from './BookingCalendar';
 
-function BookingModal({ modalIsOpen, closeModal, startDate, endDate, setStartDate, setEndDate, guests, setGuests, venueId, menuOpen }) {
-    const [message, setMessage] = useState('');
+function BookingModal({
+  modalIsOpen,
+  closeModal,
+  startDate,
+  endDate,
+  setStartDate,
+  setEndDate,
+  guests,
+  setGuests,
+  venueId,
+  menuOpen,
+}) {
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const makeBooking = () => {
-        const accessToken = localStorage.getItem("accessToken");
-        if (!accessToken) {
-            setMessage("You have to be logged in to book");
-            return;
-        }
-        
-        handleBook(startDate, endDate, guests, venueId).then(response => {
-            setMessage(response.message);
-            if (response.success) {
-                setTimeout(() => {
-                    closeModal();
-                }, 2000);
-            }
-        });
-    };
-    
-    const today = new Date(); // Get the current date
+  const makeBooking = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      setMessage('You have to be logged in to book');
+      return;
+    }
 
-    return (
-        <MS.StyledModal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            contentLabel="Booking Modal"
-            menuOpen={menuOpen}
-        >
-            <MS.ModalContent>
-                <MS.ModalHeader>Create a booking</MS.ModalHeader>
-                <MS.ModalInputGroup>
-                    <MS.ModalInput
-                        type="number"
-                        min="0"
-                        placeholder="Number of guests"
-                        onChange={event => setGuests(parseInt(event.target.value, 10))}
-                    />
+    if (!startDate || !endDate) {
+      setErrorMessage('Please select a start and end date');
+      return;
+    }
 
-                    <MS.DatePicker
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        placeholderText="Start date"
-                        dateFormat="yyyy-MM-dd"
-                        locale="en-GB"
-                        minDate={today} // Set the minimum selectable date to today
-                    />
+    if (startDate >= endDate) {
+      setErrorMessage('The end date must be after the start date');
+      return;
+    }
 
-                    <MS.DatePicker
-                        selected={endDate}
-                        onChange={date => setEndDate(date)}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        placeholderText="End date"
-                        dateFormat="yyyy-MM-dd"
-                        locale="en-GB"
-                    />
-                </MS.ModalInputGroup>
+    if (!isConsecutiveDates(startDate, endDate)) {
+      setErrorMessage('The selected dates must be consecutive');
+      return;
+    }
 
-                {message && <MS.ModalFeedback>{message}</MS.ModalFeedback>}
+    handleBook(startDate, endDate, guests, venueId).then((response) => {
+      setMessage(response.message);
+      if (response.success) {
+        setTimeout(() => {
+          closeModal();
+        }, 2000);
+      }
+    });
+  };
 
-                <MS.ModalButtonGroup>
-                    <MS.CloseModal onClick={closeModal}>Close</MS.CloseModal>
-                    <MS.ConfirmModal onClick={makeBooking}>Book now</MS.ConfirmModal>
-                </MS.ModalButtonGroup>
-            </MS.ModalContent>
-        </MS.StyledModal>
-    );
+  const isConsecutiveDates = (start, end) => {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const nextDay = new Date(startDate);
+    nextDay.setDate(nextDay.getDate() + 1);
+    return nextDay.getTime() !== endDate.getTime();
+  };
+  
+  
+  const clearError = () => {
+    setErrorMessage('');
+  };
+
+  const today = new Date(); // Get the current date
+
+  return (
+    <MS.StyledModal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Booking Modal"
+      menuOpen={menuOpen}
+    >
+      <MS.ModalContent>
+        <MS.ModalHeader>Create a booking</MS.ModalHeader>
+        <MS.ModalInputGroup>
+          <MS.ModalInput
+            type="number"
+            min="0"
+            placeholder="Number of guests"
+            onChange={(event) => setGuests(parseInt(event.target.value, 10))}
+          />
+
+          <BookingCalendar
+            venueId={venueId}
+            startDate={startDate}
+            endDate={endDate}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+          />
+
+          {/* Rest of the code */}
+        </MS.ModalInputGroup>
+
+        {errorMessage && <MS.ModalFeedback>{errorMessage}</MS.ModalFeedback>}
+        {message && <MS.ModalFeedback>{message}</MS.ModalFeedback>}
+
+        <MS.ModalButtonGroup>
+          <MS.CloseModal onClick={closeModal}>Close</MS.CloseModal>
+          <MS.ConfirmModal onClick={makeBooking}>Book now</MS.ConfirmModal>
+        </MS.ModalButtonGroup>
+      </MS.ModalContent>
+    </MS.StyledModal>
+  );
 }
 
 export default BookingModal;
