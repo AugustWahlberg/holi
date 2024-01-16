be01af801f9dcdf74fe4395e6c0695e7cdce33f4
AugustWahlberg <91577070+AugustWahlberg@users.noreply.github.com>
2024-01-16 12:14:27 +0100

    Edited searchbar colors

--- src/components/Searchbar.Styles.js
+++ src/components/Searchbar.Styles.js
@@ -8,15 +8,16 @@ export const SearchElement = styled.form`
   z-index: 2;
   padding: 5px;
   margin: 10px;
+  
 `;
 
 export const SearchInput = styled.input`
   width: 320px;
   font-weight: 600;
   font-size: 16px;
-  background: rgba(235, 241, 244, 0.8);
+  background: rgba(235, 241, 244, 0.5);
   border-radius: 40px;
-  color: rgba(0, 49, 68, 0.4);
+  color: #315867;
   border: none;
   padding: 5px;
   width: 200px;
